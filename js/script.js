290fd6bfa2a017e2073e9c60ce5fbe90d3a3d69c
Sankara Surya Tejaswi Gadepalli <97437250+tejagadepalli@users.noreply.github.com>
2025-05-26 12:50:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,72 @@
+var typed = new Typed('.typing', {
+    strings: ["Software Development"],
+    typeSpeed: 80,     
+    backSpeed: 100,     
+    loop: true
+});
+
+
+const navItems = document.querySelectorAll('.nav-item');
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navItems.forEach(nav => nav.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+    const navItems = document.querySelectorAll('.nav-item');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                navItems.forEach(navItem => {
+                    navItem.classList.remove('active');
+                    if (navItem.getAttribute('href').substring(1) === entry.target.id) {
+                        navItem.classList.add('active');
+                    }
+                });
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+
+// (function() {
+//     emailjs.init('GWbNmnXzdkXtyMcIM'); // Replace 'YOUR_USER_ID' with your EmailJS user ID
+// })();
+
+// // Function to send email
+// function sendEmail(event) {
+//     event.preventDefault();
+    
+//     var templateParams = {
+//         name: document.getElementById('name').value,
+//         email: document.getElementById('email').value,
+//         subject: document.getElementById('subject').value,
+//         message: document.getElementById('message').value
+//     };
+    
+//     emailjs.send('service_m7y71lj', 'YOUR_TEMPLATE_ID', templateParams)
+//         .then(function(response) {
+//             console.log('SUCCESS!', response.status, response.text);
+//             alert('Email sent successfully!');
+//         }, function(error) {
+//             console.log('FAILED...', error);
+//             alert('Failed to send email.');
+//         });
+// }
